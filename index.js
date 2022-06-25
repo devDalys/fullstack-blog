9a7dbd50080112cb7156c07297eb96ca3fa97f3d
Viktor <idalys1357@gmail.com>
2022-06-25 16:23:21 +0300

    init db

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+	.connect('mongodb+srv://admin:Pa-196576@cluster0.fq3jjhg.mongodb.net/?retryWrites=true&w=majority')
+	.then(() => {
+		console.log('DB ok');
+	})
+	.catch(err => console.log('DB error:', err));
+
 const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
 	res.send('Hello world');
+});
+
+app.post('/login', (req, res) => {
+	const token = jwt.sign(
+		{
+			email: req.body.email,
+			fullName: 'Вас пупкин'
+		},
+		'secret123'
+	);
+	res.json({
+		success: true,
+		token
+	});
 });
 
 app.listen(4444, err => {
