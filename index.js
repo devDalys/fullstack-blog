0a5d43aaf65548ff9b4d2a6f81bbc6d003f47f16
Viktor <idalys1357@gmail.com>
2022-07-14 21:14:24 +0300

    multer for uploads

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import multer from 'multer';
 import mongoose from 'mongoose';
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 import checkAuth from './utils/checkAuth.js';
@@ -16,6 +17,25 @@ const app = express();
 
 app.use(express.json());
 
+app.use('/uploads', express.static('uploads'));
+
+const storage = multer.diskStorage({
+	destination: (_, __, cb) => {
+		cb(null, 'uploads');
+	},
+	filename: (_, file, cb) => {
+		cb(null, file.originalname);
+	}
+});
+
+const upload = multer({ storage });
+
+app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
+	res.json({
+		url: `/uploads/${req.file.originalname}`
+	});
+});
+
 app.post('/login', loginValidation, UserController.login);
 
 app.post('/register', registerValidation, UserController.register);
@@ -24,9 +44,9 @@ app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.get('/posts', PostController.getAll);
 app.get('/posts/:id', PostController.getOne);
-app.post('/posts',checkAuth, postCreateValidation, PostController.create);
-app.delete('/posts/:id',checkAuth, PostController.remove);
-app.patch('/posts/:id', PostController.update);
+app.post('/posts', checkAuth, postCreateValidation, PostController.create);
+app.delete('/posts/:id', checkAuth, PostController.remove);
+app.patch('/posts/:id', checkAuth,postCreateValidation, PostController.update);
 
 app.listen(4444, err => {
 	if (err) {
